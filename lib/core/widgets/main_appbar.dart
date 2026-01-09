efefdf83a55341693f2762cb6216173481be4348
hasanShaaban <hsahasan16@gmail.com>
2026-01-09 15:47:04 +0300

    download chat view package

--- lib/core/widgets/main_appbar.dart
+++ lib/core/widgets/main_appbar.dart
@@ -2,7 +2,7 @@ import 'package:archilink/core/utils/app_text_style.dart';
 import 'package:archilink/core/utils/assets.dart';
 import 'package:archilink/core/widgets/app_bar_action_button.dart';
 import 'package:archilink/features/Home/presentation/views/widgets/home_page_tap_bar.dart';
-import 'package:archilink/features/post/presentation/view/post_details_view.dart';
+import 'package:archilink/features/chat/presentation/view/chat_view.dart';
 import 'package:flutter/material.dart';
 
 class MainAppBar extends StatelessWidget {
@@ -32,7 +32,7 @@ class MainAppBar extends StatelessWidget {
         AppBarActionButton(
           icon: Assets.assetsIconsMail,
           onPress: () {
-            Navigator.of(context, rootNavigator: true).pushNamed(PostDetailsView.name);
+            Navigator.of(context, rootNavigator: true).pushNamed(ChatView.name);
           },
         ),
         AppBarActionButton(
